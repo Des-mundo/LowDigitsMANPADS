217ed859f0ef3aca6838cc532c86c6c5bcc39583
Xeno426 <xmorph@gmail.com>
2024-06-29 20:44:52 -0500

    Unit liveries
    Added liveries for new units.

---
+++ Mods/tech/LowDigitMANPADS/Soldier_Redeye_LDM/digital_grey/description.lua
@@ -0,0 +1,11 @@
+livery = {
+	{"soldier_stinger", 0 ,"soldier_stinger_digital_grey",true};
+	
+}
+name = "Digital grey"
+--countries = {"USA", "ISR", "AUSAF"}
+--
+custom_args = 
+{
+	[17] = 0.6, 
+}
